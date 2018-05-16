83c5c4e4a7616968bea9faec604076420012a5cd
zyando <zyando.iron79@gmail.com>
2018-05-16 16:59:49 +0900

    First Commit

---
+++ M3MacroPlugin/Core.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Windows.Forms;
+using IronPython;
+using MikuMikuPlugin;
+
+namespace M3MacroPlugin
+{
+    public class Core : IResidentPlugin
+    {
+        public static string PluginRootPath => Application.StartupPath + "\\M3Macro\\";
+        public static string SettingPythonFilePath => PluginRootPath + "config.py";
+
+        public Guid GUID => new Guid();
+        public IWin32Window ApplicationForm { get; set; }
+        public Scene Scene { get; set; }
+
+        public string Description => "M3Macro Plugin by zyando";
+        public string Text => "M3Macro Plugin";
+        public string EnglishText => "M3Macro Plugin";
+
+        public Image Image { get; set; }
+        public Image SmallImage => Image;
+
+        public PythonExecutor Executor { get; } 
+        private HookNativeWindow HookNativeWindow { get; }
+
+        public dynamic ScriptDynamic { get; private set; }
+
+        public Core()
+        {
+            Executor = new PythonExecutor();
+            HookNativeWindow = new HookNativeWindow(this);
+        }
+
+        public void Initialize()
+        {
+            try
+            {
+                ScriptDynamic = Executor.Engine.ExecuteFile(SettingPythonFilePath, Executor.RootScope);
+                HookNativeWindow.RegisterHotKeys(ScriptDynamic.init_hotkeys());
+                HookNativeWindow.StartHook(Control.FromHandle(ApplicationForm.Handle));
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Update(float Frame, float ElapsedTime)
+        {
+
+        }
+
+        public void Disabled()
+        {
+        }
+
+        public void Enabled()
+        {
+        }
+    }
+}
---
+++ M3MacroPlugin/PythonExecutor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using IronPython.Hosting;
+using IronPython.Compiler;
+using Microsoft.Scripting.Hosting;
+using System.Windows.Forms;
+
+namespace M3MacroPlugin
+{
+    public class PythonExecutor
+    {
+        public ScriptEngine Engine { get; }
+        public ScriptScope RootScope { get; }
+
+        public PythonExecutor()
+        {
+            Engine = Python.CreateEngine();
+            RootScope = Engine.CreateScope();
+
+            RootScope.SetVariable("STARTUP_PATH", Application.StartupPath);
+        }
+
+        public ScriptScope CreateScope() => Engine.CreateScope(RootScope);
+
+        public void SetGlobalVariable(params (string name, object value)[] variables)
+        {
+            foreach (var (name, value) in variables)
+            {
+                RootScope.SetVariable(name, value);
+            }
+        }
+
+        public void SetOut(Stream stream)
+        {
+            Engine.Runtime.IO.OutputStream.Dispose();
+            Engine.Runtime.IO.SetOutput(stream, Encoding.UTF8);
+        }
+
+        public void SetOut(TextWriter writer)
+        {
+            var ms = new MemoryStream();
+            Engine.Runtime.IO.SetOutput(ms, writer);
+        }
+
+        public string FormatException(Exception e) => Engine.GetService<ExceptionOperations>().FormatException(e);
+    }
+}
